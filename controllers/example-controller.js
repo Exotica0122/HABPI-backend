776e5405d0cce3c3a771514c8ad513ee03d4628b
yennie0819 <anpeteran2@gmail.com>
2022-09-28 19:38:35 +1300

    Added JSDocs

--- controllers/example-controller.js
+++ controllers/example-controller.js
@@ -1,5 +1,9 @@
 const Example = require("../models/example");
 
+/**
+ * @author Peter An
+ * @description This is how to use JSDocs 
+ */
 const getExample = (req, res, next) => {
     return res.status(200).json({ msg: "hi" });
 };
