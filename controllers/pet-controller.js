ef3680d84f0b818bfaa2d29f430dcc9359ddf2a4
Elves00 <91095059+Elves00@users.noreply.github.com>
2022-09-29 19:37:45 +1300

    routes for getting pets by id and deleting by id

--- controllers/pet-controller.js
+++ controllers/pet-controller.js
@@ -1,37 +1,57 @@
 const mongoose = require("mongoose");
-const HttpError = require("../models/http-error")
+const HttpError = require("../models/http-error");
 const Pet = require("../models/pet");
 
+const editPet = async (req, res) => {
+  const userId = req.params.uid;
+  const { name, age, breed } = req.body;
+  // console.log(pet.name)
 
-const editPet = async(req,res)=>{
-    const userId = req.params.uid
-    const { name, age, breed } = req.body;
-    // console.log(pet.name)
-    
-    let updatedPet;
-    try{
-      updatedPet  = await Pet.findById(userId);
-    
-
-    }catch(err){
-        return next( new HttpError("Something we wrong, can't update pet",500))
-    } 
-    updatedPet.name = name;
-    updatedPet.age = age;
-    updatedPet.breed = breed;
-    try{
-        updatedPet.save();
-    }catch(err){
-        return next(
-            new HttpError("Something went wrong could not save update pet",500)
-        )
-
-    }
-    return res.status(200).json({pet:updatedPet.toObject({getters:true})});
+  let updatedPet;
+  try {
+    updatedPet = await Pet.findById(userId);
+  } catch (err) {
+    return next(new HttpError("Something we wrong, can't update pet", 500));
+  }
+  updatedPet.name = name;
+  updatedPet.age = age;
+  updatedPet.breed = breed;
+  try {
+    updatedPet.save();
+  } catch (err) {
+    return next(
+      new HttpError("Something went wrong could not save update pet", 500)
+    );
+  }
+  return res.status(200).json({ pet: updatedPet.toObject({ getters: true }) });
 };
 
-const deletePet = async (req,res)=> {
-    
+const deletePet = async (req, res) => {
+
+  const userID=req.params.uid;
+  let deletePet;
+  try {
+    deletePet = await Pet.findOneAndDelete(userID);
+  } catch (err) {
+    return next(new HttpError("Something went wrong, can't find pet", 500));
+  }
+
+  return res.status(200).json({ pet: deletePet.toObject({ getters: true }) });
+  
+};
+
+const getPetByID = async (req, res) => {
+  const userID=req.params.uid;
+  console.log(userID)
+  let foundPet;
+
+  try{
+    foundPet = await Pet.findById(userID);
+  } catch(err){
+    return next(new Error("Couldn't find pet with id:",userID))
+  }
+  console.log(foundPet)
+  res.json(foundPet.toObject({getters:true}));
 };
 
 const getPet = async (req, res) => {
@@ -47,7 +67,6 @@ const getPet = async (req, res) => {
 
 const postPet = async (req, res) => {
   const { name, age, breed } = req.body;
-  console.log(req);
   const createdPet = new Pet({
     name,
     age,
@@ -61,9 +80,11 @@ const postPet = async (req, res) => {
   }
 
   res.status(201).json({ pet: createdPet.toObject({ getters: true }) });
-
 };
 
+
 exports.getPet = getPet;
+exports.getPetByID=getPetByID;
 exports.postPet = postPet;
 exports.editPet = editPet;
+exports.deletePet=deletePet;
--- routes/pet-route.js
+++ routes/pet-route.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const petController = require("../controllers/pet-controller");
 
 router.get("/", petController.getPet);
+router.get("/:uid",petController.getPetByID)
 router.post("/", petController.postPet)
 router.put("/:uid",petController.editPet)
+router.delete("/:uid",petController.deletePet)
 
 module.exports = router;
