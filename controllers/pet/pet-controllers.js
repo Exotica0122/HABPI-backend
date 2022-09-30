2597aa5ddeb07a75f142acefb683c748bc03c324
Exotica0122 <75343007+Exotica0122@users.noreply.github.com>
2022-10-01 10:53:59 +1300

    getting service by id works

--- controllers/pet/pet-controllers.js
+++ controllers/pet/pet-controllers.js
@@ -3,86 +3,93 @@ const HttpError = require("../../models/http-error");
 const Pet = require("../../models/pet");
 
 const editPetId = async (req, res, next) => {
-  const petId = req.params.pid;
-  const { name, age, breed } = req.body;
+    const petId = req.params.pid;
+    const { name, age, breed } = req.body;
 
-  let updatedPet;
-  try {
-    updatedPet = await Pet.findById(petId);
-  } catch (err) {
-    return next(new HttpError("Something we wrong, can't update pet", 500));
-  }
-  updatedPet.name = name;
-  updatedPet.age = age;
-  updatedPet.breed = breed;
-  try {
-    updatedPet.save();
-  } catch (err) {
-    return next(
-      new HttpError("Something went wrong could not save updated pet", 500)
-    );
-  }
-  return res.status(200).json({ pet: updatedPet.toObject({ getters: true }) });
+    let updatedPet;
+    try {
+        updatedPet = await Pet.findById(petId);
+    } catch (err) {
+        return next(new HttpError("Something we wrong, can't update pet", 500));
+    }
+    updatedPet.name = name;
+    updatedPet.age = age;
+    updatedPet.breed = breed;
+    try {
+        updatedPet.save();
+    } catch (err) {
+        return next(
+            new HttpError(
+                "Something went wrong could not save updated pet",
+                500
+            )
+        );
+    }
+    return res
+        .status(200)
+        .json({ pet: updatedPet.toObject({ getters: true }) });
 };
 
 const deletePet = async (req, res, next) => {
-  const petId = req.params.pid;
-  let petToDelete;
-  try {
-    petToDelete = await Pet.findById(petId);
-  } catch (err) {
-    return next(
-      new HttpError("Something went wrong finding pet to delete", 500)
-    );
-  }
+    const petId = req.params.pid;
+    let petToDelete;
+    try {
+        petToDelete = await Pet.findById(petId);
+    } catch (err) {
+        return next(
+            new HttpError("Something went wrong finding pet to delete", 500)
+        );
+    }
 
-  try {
-    petToDelete.remove();
-  } catch (err) {
-    return next(new HttpError("Something went wrong removing pet", 500));
-  }
-  return res.status(200).json({ pet: petToDelete.toObject({ getters: true }) });
+    try {
+        petToDelete.remove();
+    } catch (err) {
+        return next(new HttpError("Something went wrong removing pet", 500));
+    }
+    return res
+        .status(200)
+        .json({ pet: petToDelete.toObject({ getters: true }) });
 };
 
 const getPetByID = async (req, res, next) => {
-  const petId = req.params.pid;
-  let foundPet;
+    const petId = req.params.pid;
+    let foundPet;
 
-  try {
-    foundPet = await Pet.findById(petId);
-  } catch (err) {
-    return next(new Error("Couldn't find pet with id:", petId));
-  }
+    try {
+        foundPet = await Pet.findById(petId);
+    } catch (err) {
+        return next(new Error("Couldn't find pet with id:", petId));
+    }
 
-  res.json({ pet: foundPet.toObject({ getters: true }) });
+    res.json({ pet: foundPet.toObject({ getters: true }) });
 };
 
 const getAllPets = async (req, res, next) => {
-  let pets;
-  try {
-    pets = await Pet.find();
-  } catch (err) {
-    return next(new Error("Couldn't find Pets"));
-  }
-  res.json({ pets: pets.map((pet) => pet.toObject({ getters: true })) });
+    let pets;
+    try {
+        pets = await Pet.find();
+    } catch (err) {
+        return next(new Error("Couldn't find Pets"));
+    }
+    res.json({ pets: pets.map((pet) => pet.toObject({ getters: true })) });
 };
 
 const createPet = async (req, res, next) => {
-  const { name, age, breed } = req.body;
+    const { name, age, breed } = req.body;
 
-  const createdPet = new Pet({
-    name,
-    age,
-    breed,
-  });
+    const createdPet = new Pet({
+        name,
+        age,
+        breed,
+    });
 
-  try {
-    createdPet.save();
-  } catch (err) {
-    return next(new HttpError("Pet creation failed", 500));
-  }
+    try {
+        createdPet.save();
+    } catch (err) {
+        return next(new HttpError("Pet creation failed", 500));
+    }
 
-  res.status(201).json({ pet: createdPet.toObject({ getters: true }) });
+    res.status(201).json({ pet: createdPet.toObject({ getters: true }) });
 };
 
 exports.getAllPets = getAllPets;
--- controllers/service/service-contollers.js
+++ controllers/service/service-contollers.js
@@ -19,28 +19,26 @@ const getAllService = async (req, res, next) => {
 };
 
 const getServiceByJobType = async (req, res, next) => {
-    console.log(req.params)
     const jobtype = req.params.jobtype;
-    const job=req.body;
-    console.log(jobtype)
-    console.log(job)
-    
-    
+    const job = req.body;
+    console.log(jobtype);
+    console.log(job);
+
     let foundServices;
-  
+
     try {
-        foundServices = await Service.find({"jobType":jobtype})
+        foundServices = await Service.find({ jobType: jobtype });
     } catch (err) {
-      return next(new Error("Couldn't find pet with id:", petId));
+        return next(new Error("Couldn't find pet with id:", petId));
     }
-  
-    console.log(foundServices)
-    res.json({
+
+    console.log(foundServices);
+    return res.json({
         foundServices: foundServices.map((service) =>
             service.toObject({ getters: true })
         ),
     });
-  };
+};
 
 const postService = async (req, res, next) => {
     const { title, jobType, minPrice, maxPrice } = req.body;
@@ -56,12 +54,32 @@ const postService = async (req, res, next) => {
     try {
         createdService.save();
     } catch (err) {
-        return next(new HttpError("Creating new service failed, please try again.", 500));
+        return next(
+            new HttpError("Creating new service failed, please try again.", 500)
+        );
     }
 
-    res.status(201).json({ service: createdService.toObject({ getters: true }) });
+    return res.status(201).json({
+        service: createdService.toObject({ getters: true }),
+    });
+};
+
+const getServiceById = async (req, res, next) => {
+    const serviceId = req.params.sid;
+    let foundService;
+
+    try {
+        foundService = await Service.findById(serviceId);
+    } catch (err) {
+        new HttpError("Finding service by id failed, please try again", 500);
+    }
+
+    return res
+        .status(200)
+        .json({ service: foundService.toObject({ getters: true }) });
 };
 
 exports.getAllService = getAllService;
 exports.postService = postService;
-exports.getServiceByJobType=getServiceByJobType;
+exports.getServiceByJobType = getServiceByJobType;
+exports.getServiceById = getServiceById;
